36972e123ad4c3799fa28045a9f441f99c36a5f3
tossougbo-mariel <marielhinson22@gmail.com>
2025-12-29 15:49:22 +0100

    implementation des routes registers login et logout

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { UserController } from "#controllers/user.controller";
+import { AuthController } from "#controllers/auth.controller";
 import { asyncHandler } from "#lib/async-handler";
+import { auth } from "#middlewares/auth";
 
 const router = Router(***REMOVED***;
 
-// Inscription et Connexion
-router.post("/register", asyncHandler(UserController.register***REMOVED******REMOVED***;
-router.post("/login", asyncHandler(UserController.login***REMOVED******REMOVED***;
+// Routes publiques
+router.post("/register", asyncHandler(AuthController.register***REMOVED******REMOVED***;
+router.post("/login", asyncHandler(AuthController.login***REMOVED******REMOVED***;
+router.post("/refresh", asyncHandler(AuthController.refresh***REMOVED******REMOVED***;
+
+// Routes protégées (nécessitent authentification***REMOVED***
+router.post("/logout", auth, asyncHandler(AuthController.logout***REMOVED******REMOVED***;
 
 export default router;
