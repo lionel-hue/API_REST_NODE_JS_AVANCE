ef9a96fabb69717c7e107d7fdaf22d1a6acfc0f1
lionel sisso <sissolinel@gmail.com>
2025-12-31 23:23:54 +0100

    github oauth working correctly

--- src/lib/oauth.js
+++ src/lib/oauth.js
@@ -54,7 +54,8 @@ export function initializePassportStrategies(***REMOVED*** {
         clientSecret: config.GITHUB_CLIENT_SECRET,
         callbackURL: `${config.APP_URL}/api/oauth/github/callback`,
         passReqToCallback: true,
-      },
+        scope : ["user.email"]
+      },``
       (req, accessToken, refreshToken, profile, done***REMOVED*** => {
         logger.info(`GitHub OAuth callback received for profile: ${profile.id}`***REMOVED***;
         
